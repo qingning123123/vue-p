d0791c1f49935252929ff302e947460f3bfcb258
qingning123123 <780762453@qq.com>
2024-11-10 21:08:39 +0800

    按钮组件完成

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,15 @@
 <script setup>
-
+import ViButton from './components/ViButton.vue'
 </script>
 
 <template>
   <div>
-    <button class="but but1">59s后自动取消</button>
+    <!-- <button class="but but1">59s后自动取消</button>
     <button class="but but2">下单</button>
-    <button class="but3">上传</button>
+    <button class="but3">上传</button> -->
+
+    <ViButton :disabled="true" text="59s后自动取消"></ViButton>
+    <ViButton text="下单"></ViButton>
+    <ViButton size="small" text="上传"></ViButton>
   </div>
 </template>
-
-<style scoped>
-.but {
-  width: 166px;
-  height: 44px;
-  border-radius: 4px;
-  font-size: 16px;
-  line-height: 16px;
-}
-
-.but1 {
-  background:  #E7E8EA;
-  color: rgba(125, 145, 157, 0.50);
-}
-
-.but2 {
-  margin-left: 10px;
-  margin-right: 10px;
-  background: #0062F8;
-  color: #FFF;
-}
-
-.but3 {
-  width: 72px;
-  height: 28px;
-  border-radius: 6px;
-  background: #0062F8;
-  color: #FFF;
-  font-size: 12px;
-  line-height: 12px;
-}
-
-</style>
---
+++ src/components/ViButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <button :class="{but1: disabled , but2: !disabled ,but3: size === 'small', but: size !== 'small'}">
+    {{ text }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  text: String,
+  disabled: Boolean,
+  size: String,
+})
+
+</script>
+
+<style scoped>
+.but {
+  width: 166px;
+  height: 44px;
+  border-radius: 4px;
+  font-size: 16px;
+  line-height: 16px;
+}
+
+.but1 {
+  background: #E7E8EA;
+  color: rgba(125, 145, 157, 0.50);
+}
+
+.but2 {
+  margin-left: 10px;
+  margin-right: 10px;
+  background: #0062F8;
+  color: #FFF;
+}
+
+.but3 {
+  width: 72px;
+  height: 28px;
+  border-radius: 6px;
+  background: #0062F8;
+  color: #FFF;
+  font-size: 12px;
+  line-height: 12px;
+}
+
+</style>
